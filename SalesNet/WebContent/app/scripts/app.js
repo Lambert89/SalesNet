5f0a356ac789d0abd0804af1529d57cfc80c33a0
jason.lambert.89@gmail.com <jason.lambert.89@gmail.com>
2015-01-13 18:36:33 +0800

    updated.

--- SalesNet/WebContent/app/scripts/app.js
+++ SalesNet/WebContent/app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
-  .module('webContentApp', [
-    'ngMessages', 'angularFileUpload', 'oitozero.ngSweetAlert'
-  ]);
+angular.module('webContentApp', [ 'ngMessages', 'angularFileUpload', 'oitozero.ngSweetAlert' ]).config(
+    function($sceDelegateProvider){
+    	$sceDelegateProvider.resourceUrlWhitelist(['self']);
+    });
--- SalesNet/WebContent/app/scripts/controllers/user.js
+++ SalesNet/WebContent/app/scripts/controllers/user.js
@@ -7,13 +7,17 @@
  * # UserCtrl
  * Controller of the webContentApp
  */
-angular.module('webContentApp')
-  .controller('UserCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }).controller("ViewerCtrl", function($scope) {
-	  
-  });
+angular.module('webContentApp').controller('UserCtrl', function($scope){
+    $scope.awesomeThings = [ 'HTML5 Boilerplate', 'AngularJS', 'Karma' ];
+}).controller("ViewerCtrl", function($scope, $http){
+    $scope.generateUrl = function(baseUrl, documentName){
+        return baseUrl + "/ViewerJS/#../api/download/" + documentName;
+    };
+    $http.get('api/documents').success(function(data, status, headers, config){
+        $scope.documents = data.documents;
+        $scope.baseUrl = data.baseUrl;
+        $scope.isError = false;
+    }).error(function(data, status, headers, config){
+        $scope.isError = true;
+    });
+});
