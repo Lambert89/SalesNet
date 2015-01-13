de4f401faab3c30779f372a68f0615bce50002e7
Lambert Yan <jason.lambert.89@gmail.com>
2015-01-14 01:01:44 +0800

    implemented for preview.

--- SalesNet/WebContent/app/scripts/controllers/user.js
+++ SalesNet/WebContent/app/scripts/controllers/user.js
@@ -3,21 +3,24 @@
 /**
  * @ngdoc function
  * @name webContentApp.controller:UserCtrl
- * @description
- * # UserCtrl
- * Controller of the webContentApp
+ * @description # UserCtrl Controller of the webContentApp
  */
-angular.module('webContentApp').controller('UserCtrl', function($scope){
-    $scope.awesomeThings = [ 'HTML5 Boilerplate', 'AngularJS', 'Karma' ];
-}).controller("ViewerCtrl", function($scope, $http){
-    $scope.generateUrl = function(baseUrl, documentName){
-        return baseUrl + "/ViewerJS/#../api/download/" + documentName;
-    };
-    $http.get('api/documents').success(function(data, status, headers, config){
-        $scope.documents = data.documents;
-        $scope.baseUrl = data.baseUrl;
-        $scope.isError = false;
-    }).error(function(data, status, headers, config){
-        $scope.isError = true;
-    });
+angular.module('webContentApp').controller('UserCtrl', function($scope) {
+	$scope.awesomeThings = [ 'HTML5 Boilerplate', 'AngularJS', 'Karma' ];
+}).controller("ViewerCtrl", function($scope, $http, $element) {
+	$scope.generateUrl = function(baseUrl, documentName, $event) {
+                console.log("clicking the link: " + documentName);
+		$scope.previewDocumentUrl = baseUrl + "/ViewerJS/#../api/download/" + documentName;
+                $($element).find("iframe")[0].contentWindow.location.reload();
+	};
+	$http.get('api/documents').success(function(data, status, headers, config) {
+		$scope.documents = data.documents;
+		$scope.baseUrl = data.baseUrl;
+		$scope.isError = false;
+		if ($scope.documents.length > 0) {
+			$scope.previewDocumentUrl = $scope.baseUrl + "/ViewerJS/#../api/download/" + $scope.documents[0];
+		}
+	}).error(function(data, status, headers, config) {
+		$scope.isError = true;
+	});
 });
